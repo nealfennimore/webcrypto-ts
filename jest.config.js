4514196b4e329ad6bd383611b24509893bd0e479
Neal Fennimore <hi@neal.codes>
2022-08-03 16:48:30 -0400

    Add in coverage support

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,18 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFiles: ['<rootDir>/jest.setup.js'],
-  testTimeout: 10000,
-  globals: {
-    'ts-jest': {
-        "encode": true,
-        "decode": true,
-    }
-  }
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFiles: ['<rootDir>/jest.setup.js'],
+    testTimeout: 10000,
+    globals: {
+        'ts-jest': {
+            "encode": true,
+            "decode": true,
+        }
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "./src/**/*.ts",
+        "!**/__tests__/**",
+    ],
 };
