4f1d5322b5028c00a4b7e25af5bd02c10174e970
Neal Fennimore <hi@neal.codes>
2022-08-15 10:40:39 -0400

    Update to combine iv and salt into random

--- src/iv.ts
+++
@@ -1,7 +0,0 @@
-import { Random } from "./random.js";
-
-export namespace IV {
-    export async function generate(): Promise<Uint8Array> {
-        return await Random.getValues(16);
-    }
-}
--- src/random.ts
+++ src/random.ts
@@ -6,4 +6,19 @@ export namespace Random {
             await WebCrypto._crypto
         ).getRandomValues(new Uint8Array(length));
     }
+
+    export namespace IV {
+        export async function generate(
+            length: number = 16
+        ): Promise<Uint8Array> {
+            return await Random.getValues(length);
+        }
+    }
+    export namespace Salt {
+        export async function generate(
+            length: number = 16
+        ): Promise<Uint8Array> {
+            return await Random.getValues(length);
+        }
+    }
 }
