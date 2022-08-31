5c4d0e165c58bedb0c2a8bcbe38d7d423a50c3d0
Neal Fennimore <hi@neal.codes>
2022-08-31 10:49:52 -0400

    Update random examples

--- src/random.ts
+++ src/random.ts
@@ -5,6 +5,13 @@
 
 import * as WebCrypto from "./webcrypto.js";
 
+/**
+ * Generate random values
+ * @example
+ * ```ts
+ * const values = await Random.getValues(16);
+ * ```
+ */
 export async function getValues(length: number): Promise<Uint8Array> {
     return await (
         await WebCrypto._crypto
@@ -19,7 +26,7 @@ export namespace IV {
      * Generate an initialization vector. Defaults to 16 bytes.
      * @example
      * ```ts
-     * const iv = await IV.generate();
+     * const iv = await Random.IV.generate();
      * ```
      */
     export async function generate(length: number = 16): Promise<Uint8Array> {
@@ -35,7 +42,7 @@ export namespace Salt {
      * Generate a salt. Defaults to 16 bytes.
      * @example
      * ```ts
-     * const salt = await Salt.generate();
+     * const salt = await Random.Salt.generate();
      * ```
      */
     export async function generate(length: number = 16): Promise<Uint8Array> {
@@ -51,7 +58,7 @@ export namespace UUID {
      * Generate a UUID.
      * @example
      * ```ts
-     * const uuid = await UUID.generate();
+     * const uuid = await Random.UUID.generate();
      * ```
      */
     export async function generate(): Promise<string> {
