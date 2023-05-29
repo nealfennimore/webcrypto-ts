3e172a3ace8b8819392f6a57fe92a7be5ef2d53b
Neal Fennimore <hi@neal.codes>
2023-05-29 17:49:57 -0400

    Update proxy signatures

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,13 +3,14 @@
  * @module
  */
 
-export interface ProxiedPubCryptoKey<T extends CryptoKey> {
+export interface ProxiedCryptoKey<T extends CryptoKey> {
     self: T;
 }
+export interface ProxiedPubCryptoKey<T extends CryptoKey>
+    extends ProxiedCryptoKey<T> {}
 
-export interface ProxiedPrivCryptoKey<T extends CryptoKey> {
-    self: T;
-}
+export interface ProxiedPrivCryptoKey<T extends CryptoKey>
+    extends ProxiedCryptoKey<T> {}
 
 export interface ProxiedCryptoKeyPair<
     TKeyPair extends CryptoKeyPair,
@@ -22,23 +23,17 @@ export interface ProxiedCryptoKeyPair<
     privateKey: TProxPrivKey;
     publicKey: TProxPubKey;
 }
-export function proxifyPubKey<
-    TPubKey extends CryptoKey,
-    TProxPubKey extends ProxiedPubCryptoKey<TPubKey>
->(handler: ProxyHandler<TPubKey>) {
-    return function _proxifyPubKey(privKey: TPubKey) {
-        return new Proxy<TPubKey, TProxPubKey>(privKey, handler);
+export function proxifyKey<
+    TKey extends CryptoKey,
+    TProxKey extends ProxiedCryptoKey<TKey>
+>(handler: ProxyHandler<TKey>) {
+    return function _proxifyKey(key: TKey) {
+        return new Proxy<TKey, TProxKey>(key, handler);
     };
 }
 
-export function proxifyPrivKey<
-    TPrivKey extends CryptoKey,
-    TProxPrivKey extends ProxiedPrivCryptoKey<TPrivKey>
->(handler: ProxyHandler<TPrivKey>) {
-    return function _proxifyPrivKey(privKey: TPrivKey) {
-        return new Proxy<TPrivKey, TProxPrivKey>(privKey, handler);
-    };
-}
+export const proxifyPubKey = proxifyKey;
+export const proxifyPrivKey = proxifyKey;
 
 export interface ProxyKeyPairHandlers<
     TPrivKey extends CryptoKey,
